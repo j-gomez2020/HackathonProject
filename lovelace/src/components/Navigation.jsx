1b2226803247f873fadb7138e09cc14926c1c8f4
Noelle Szombathy <business.noellered@gmail.com>
2021-04-24 18:35:30 -0700

    add color to progress bar

--- lovelace/src/components/Navigation.jsx
+++ lovelace/src/components/Navigation.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import {Link, useHistory} from 'react-router-dom';
+import {Link, useHistory, useLocation} from 'react-router-dom';
 
 const Navigation = ({ prevStep, nextStep }) => {
     const history = useHistory();
+    const location = useLocation();
     return(
         <div className="navigation-container">
             {prevStep ? <button onClick={()=>{history.replace(prevStep)}} className="button outline">prev</button> : ''}
             <div className="progress-bar-container">
-                <div className="bar">1</div> 
-                <div className="bar">2</div> 
-                <div className="bar">3</div> 
+                <div className={`bar ${location.pathname === '/steps/1' ? 'active' : ''}`}>&nbsp;</div> 
+                <div className={`bar ${location.pathname === '/steps/2' ? 'active' : ''}`}>&nbsp;</div> 
+                <div className={`bar ${location.pathname === '/steps/3' ? 'active' : ''}`}>&nbsp;</div> 
             </div>
             {nextStep ? <button onClick={()=>{history.replace(nextStep)}} className="button primary">next</button> : <button onClick={()=> history.replace('/results')} className="primary button">show results</button>}
         </div>
